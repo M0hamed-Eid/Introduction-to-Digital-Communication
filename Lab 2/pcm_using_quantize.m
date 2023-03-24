53a1fe6c21bf12200dc0e4488e9da0d680415495
Seif Zakaria <127408716+Seifo321@users.noreply.github.com>
2023-03-24 17:43:07 +0200

    Update pcm_using_quantize.m

--- Lab 2/pcm_using_quantize.m
+++ Lab 2/pcm_using_quantize.m
@@ -1,33 +1,33 @@
 clear, clc ,close all;
 
-% Parameters
-A = 1;                      % amplitude of sinusoidal wave
-f = 2;                      % frequency of sinusoidal wave
-Fs = 4000;                  % sampling frequency
-m = 7;                      % total number of bits, including the sign bit
-n = floor((m-1)/2);         % number of bits for integer value and fraction part
+% Define the parameters
+f_s = 4000;         % Sampling frequency (Hz)
+f_sig = 2;          % Signal frequency (Hz)
+A_sig = 1;          % Signal amplitude (V)
 
-% Generate the signal
-t1 = 0:1/Fs:1/f;             % time vector
-x = A*sin(2*pi*f*t1);        % sinusoidal wave
+% Generate the time vector
+t = 0:1/f_s:1-1/f_s;
 
-% Quantize the signal using quantize command
-vmax = max(abs(x));         % maximum absolute value of input signal (m_P)
-q = 2*vmax/(2^n);           % quantization step size
-% Apply quantize function to the scaled input
-xq = quantize(x, fi([], true, 16, 0));
+% Generate the sinusoidal signal
+x = A_sig*sin(2*pi*f_sig*t);
 
-% Convert the quantized samples to binary
-x_bin = dec2bin(xq);
+% Define the number of bits for quantization
+n_bits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-% Calculate the mean square quantization error
-n_values = [3, 4, 5, 6, 7, 8, 9, 10];
-mse = zeros(size(n_values));
-for i = 1:length(n_values)
-    n = n_values(i);
-    m = 2*n + 1;
-    quantized_signal = quantize(x, fi([], true, 16, 0));
-    quantization_error = x - quantized_signal;
-    mse(i) = sum(quantization_error.^2)/length(quantization_error);
-    fprintf('n = %d, MSE = %f\n', n, mse(i));
+% Initialize the mean squared error (MSE) vector
+mse = zeros(size(n_bits));
+
+% Loop over each number of bits
+for i = 1:length(n_bits)
+    n = n_bits(i);              % Number of bits
+    L = 2^n;                    % Number of quantization levels
+    step_size = (2*A_sig)/L;    % Quantization step size
+    partition = [-1:step_size:1];   % Define the partition for the quantization
+    start = -A_sig - step_size;      % Define the start of the codebook
+    codebook = [start:step_size:1];  % Define the codebook for the quantization
+    [index,x_q,distor_linear] = quantiz(x,partition, codebook);  % Quantize the signal
+    quantization_error = x - x_q;   % Calculate the quantization error
+    mse(i) = sum(quantization_error.^2)/length(quantization_error); % Calculate the MSE
+    fprintf('n = %d, MSE = %f\n', n, mse(i));  % Print the results
 end
+

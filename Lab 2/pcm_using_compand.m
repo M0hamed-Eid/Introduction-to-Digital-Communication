848ece037bf83430957df3cffebb629d3fdff794
Seif Zakaria <127408716+Seifo321@users.noreply.github.com>
2023-03-24 22:55:00 +0200

    Update pcm_using_compand.m

--- Lab 2/pcm_using_compand.m
+++ Lab 2/pcm_using_compand.m
@@ -15,13 +15,13 @@ x = A*sin(2*pi*f*t1);        % sinusoidal wave
 vmax = max(abs(x));         % maximum absolute value of input signal (m_P)
 mu = 255;                   % companding law parameter
 compand_type = 'mu-law';    % companding law type
-xq = compand(x,mu,vmax,compand_type); % quantize signal using compand command
-xq = fi(xq,1,m,n);          % convert to fi object
-xq = double(xq);            % convert to double precision for further processing
+% xq = compand(x,mu,vmax,compand_type); % quantize signal using compand command
+% xq = fi(xq,1,m,n);          % convert to fi object
+% xq = double(xq);            % convert to double precision for further processing
 
 % Convert the quantized samples to binary
-q = 2*vmax/(2^n);           % quantization step size
-x_bin = de2bi((xq + 1)*(2/q), m, 'left-msb');
+% q = 2*vmax/(2^n);           % quantization step size
+% x_bin = de2bi((xq + 1)*(2/q), m, 'left-msb');
 
 % Calculate the mean square quantization error
 n_values = [3, 4, 5, 6, 7, 8, 9, 10];
@@ -29,8 +29,10 @@ mse = zeros(size(n_values));
 for i = 1:length(n_values)
     n = n_values(i);
     m = 2*n + 1;
-    quantized_signal = double(fi(x,1,m,n));
-    quantization_error = x - quantized_signal;
+    xq = compand(x,mu,vmax,compand_type);
+    xq = fi(xq,1,m,n);          % convert to fi object
+    xq = double(xq);  
+    quantization_error = x -xq;
     mse(i) = sum(quantization_error.^2)/length(quantization_error);
     fprintf('n = %d, MSE = %f\n', n, mse(i));
 end
